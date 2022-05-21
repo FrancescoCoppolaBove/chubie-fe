84199f099c314597fd10b9684549d6a2c02601d1
Francesco Coppola Bove <francesco.coppolabove@assistdigital.com>
2022-05-21 15:41:17 +0200

    first commit

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import styles from "../styles/Home.module.css";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <div>
+        <h1>CHUBIE</h1>
+      </div>
+    </div>
+  );
+}
