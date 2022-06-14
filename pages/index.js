478f7c0631f3e3c3f71e764ec8ac7d17c134fbcf
Francesco Coppola Bove <francesco.coppolabove@assistdigital.com>
2022-06-14 15:55:30 +0200

    fixed getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -2,22 +2,10 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Main from '../components/home-page/Main';
 import TopCreators from '../components/home-page/TopCreators';
-import {
-  getTopCreators,
-  getLatestUploadCreators,
-  getPopularCreators,
-  getHotBids,
-  getHotCollections
-} from '../lib/fetch-utils';
 import Popular from '../components/home-page/Popular';
 import Description from '../components/home-page/Description';
 import HotBid from '../components/home-page/HotBid';
 import HotCollections from '../components/home-page/HotCollections';
-import { hotCollectionsMock } from '../assets/data/hotCollectionsMock';
-import { hotBidsMock } from '../assets/data/hotBidsMock';
-import { popularMock } from '../assets/data/popularMock';
-import { latestUploadCreatorsMock } from '../assets/data/latestUploadCreatorsMock';
-import { topCreatorsMock } from '../assets/data/topCreatorsMock';
 import Discover from '../components/home-page/Discover';
 
 const style = {
@@ -43,12 +31,3 @@ export default function Home({ creators, latestUploadCreators, popular, hotBids,
     </div>
   );
 }
-
-export const getStaticProps = async () => {
-  // const creators = await getTopCreators();
-  // const latestUploadCreators = await getLatestUploadCreators();
-  // const popular = await getPopularCreators();
-  // const hotBids = await getHotBids();
-  // const hotCollections = await getHotCollections();
-  // return { props: { hotCollections } };
-};
